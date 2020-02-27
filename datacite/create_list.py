7dda13a5052f33ce4adbe127a2e430346f38193f
Peter Vos <pjmvos@gmail.com>
2020-02-27 20:16:31 +0100

    first complete run

---
+++ datacite/create_list.py
@@ -0,0 +1,33 @@
+'''
+Title: Use the data gathered by datacite_rest_api.json to create a list you can filter in Excel
+Author: Peter Vos
+
+'''
+from datetime import datetime
+import json
+import csv
+
+
+def create_csv(json_file):
+    today_str = datetime.now().strftime('%Y%m%d')
+    data = json.load(open(json_file))
+    pubcount = {}
+    with open(json_file.replace('.json', '.csv'), mode='w', newline='', encoding='utf-8') as datafile:
+        w = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        w.writerow(['doi', 'name', 'affiliation', 'publisher'])
+        for id in data:
+            record = data[id]
+
+            publisher = record['attributes']['publisher']
+            if publisher in pubcount:
+                pubcount[publisher] = pubcount[publisher] + 1
+            else:
+                pubcount[publisher] = 1
+            c=record['attributes']['creators'][0]
+            for a in c['affiliation']:
+                print(a)
+                w.writerow([id, c['name'], a, publisher])
+    print(pubcount)
+
+
+create_csv('datacite20200227.json')
--- datacite/datacite_rest_api.py
+++ datacite/datacite_rest_api.py
@@ -1,17 +1,37 @@
-# https://api.datacite.org/dois?query=creators.affiliation.name:%22Vrije%20Universiteit%20Amsterdam%22&resource-type-id=dataset
-# https://api.datacite.org/dois?query=creators.affiliation.name:%22Vrije+UniversiteitAmsterdam%22&page=1&size=100&resource-type-id=dataset
-# https://search.datacite.org/works?query=creators.affiliation.name%3AVU*&affiliation-id=https%3A%2F%2Fror.org%2F008xxew50
-# https://support.datacite.org/docs/api
-import json
+'''
+Title: Get records from datacite api by author affiliation
+Author: Peter Vos
 
-import requests
+https://support.datacite.org/docs/api
+
+No api key is needed. API is very slow. Max page size is 25.
+There are two ways to get authors by affiliation:
+1. By string: query=creators.affiliation.name:<query string>, note this is case-sensitive
+2. By affiliation-id, this is done by ROR, see https://ror.org. The VU has id https://ror.org/008xxew50
+'''
+from datetime import datetime
+import time
+import json
+import requests, requests_cache
+
+requests_cache.install_cache(cache_name='datacite_requests_cache', allowable_methods=('GET', 'POST'))
 
 
 def _do_request(query, page=1, size=25, type="dataset", affiliation_id=''):
+    '''
+    Do the query request
+
+    :param query:
+    :param page:
+    :param size:
+    :param type:
+    :param affiliation_id:
+    :return: json result in a dict
+    '''
     params = {
         "query": query,
-        "page": page,
-        "size": size,
+        "page[number]": page,
+        "page[size]": size,
         "resource-type-id": type,
         "affiliation-id": affiliation_id
     }
@@ -21,11 +41,26 @@ def _do_request(query, page=1, size=25, type="dataset", affiliation_id=''):
 
 
 def _store(data, filename):
+    '''
+    Store the data dict as a json file
+
+    :param data: dict
+    :param filename: string
+    :return:
+    '''
     with open(filename, 'w') as f:
         json.dump(data, f, indent=1)
 
 
 def get_records(query, data, affiliation_id=''):
+    '''
+    Do the initial query, and loop through the pages. Append the records to data only if the first author affiliation contains VU or Vrije
+
+    :param query:
+    :param data:
+    :param affiliation_id:
+    :return:
+    '''
     d = _do_request(query, affiliation_id=affiliation_id)
     print('total: %s' % d['meta']['total'])
     print('total pages: %s' % d['meta']['totalPages'])
@@ -33,16 +68,24 @@ def get_records(query, data, affiliation_id=''):
         print(f'page {page}')
         d = _do_request(query=query, page=page, affiliation_id=affiliation_id)
         for record in d['data']:
-            data[record['id']] = record
+            # check first author
+            c=record['attributes']['creators'][0]
+            # a bit of extra cleaning
+            for a in c['affiliation']:
+                if a.lower().find('vu ') > -1 or a.lower().find('vrije') > -1:
+                    data[record['id']] = record
     return data
 
 
+today_str = datetime.now().strftime('%Y%m%d')
 data = {}
 
-affs=['"Vrije Universiteit Amsterdam"','"VU University Amsterdam"','VU*','Vrije*']
+# cast a wide net, we will filter later
+affs = ['*vrije*', '*vu*', '*free*', '*VU*', '*Vrije*', '*Free*']
 for affiliation in affs:
     query = f'creators.affiliation.name:{affiliation}'
-    data=get_records(query, data)
+    data = get_records(query, data)
 
+# by ROR, although we should already have those
 data = get_records('*', data, affiliation_id='https://ror.org/008xxew50')
-_store(data, 'datacite.json')
+_store(data, f'datacite{today_str}.json')
