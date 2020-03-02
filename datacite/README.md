3d12eaea6b083770f54584c3977f2392e4168ddc
Peter Vos <pjmvos@gmail.com>
2020-03-02 18:11:24 +0100

    add software

--- datacite/README.md
+++ datacite/README.md
@@ -1,4 +1,5 @@
 Search by affiliation with `creators.affiliation.name`
+
 https://api.datacite.org/dois?query=creators.affiliation.name:%22Vrije%20Universiteit%20Amsterdam%22&resource-type-id=dataset
 
 No key or authentication needed.
--- datacite/datacite_rest_api.py
+++ datacite/datacite_rest_api.py
@@ -52,7 +52,7 @@ def _store(data, filename):
         json.dump(data, f, indent=1)
 
 
-def get_records(query, data, affiliation_id='', max_author=1):
+def get_records(query, data, affiliation_id='', max_author=1, type="dataset"):
     '''
     Do the initial query, and loop through the pages. Append the records to data only if the first author affiliation contains VU or Vrije
 
@@ -61,16 +61,18 @@ def get_records(query, data, affiliation_id='', max_author=1):
     :param affiliation_id:
     :return:
     '''
-    d = _do_request(query, affiliation_id=affiliation_id)
+    d = _do_request(query, affiliation_id=affiliation_id, type=type)
     print('total: %s' % d['meta']['total'])
     print('total pages: %s' % d['meta']['totalPages'])
     for page in range(1, d['meta']['totalPages']):
         print(f'page {page}')
-        d = _do_request(query=query, page=page, affiliation_id=affiliation_id)
+        d = _do_request(query=query, page=page, affiliation_id=affiliation_id, type=type)
         for record in d['data']:
             # check first author
-            for n in [0,max_author-1]:
+            n=0
+            while n < max_author and n < len(record['attributes']['creators']):
                 c = record['attributes']['creators'][n]
+                n=n+1
                 # a bit of extra cleaning
                 for a in c['affiliation']:
                     if (a.lower().find('vu ') > -1 or a.lower().find('vrije') > -1) and (a.lower().find(
@@ -80,14 +82,16 @@ def get_records(query, data, affiliation_id='', max_author=1):
 
 
 today_str = datetime.now().strftime('%Y%m%d')
-data = {}
+for type in ['dataset','software']:
+    data = {}
 
-# cast a wide net, we will filter later
-affs = ['*vrije*', '*vu*', '*VU*', '*Vrije*']
-for affiliation in affs:
-    query = f'creators.affiliation.name:{affiliation}'
-    data = get_records(query, data)
+    # cast a wide net, we will filter later
+    affs = ['*vrije*', '*vu*', '*VU*', '*Vrije*']
+    for affiliation in affs:
+        query = f'creators.affiliation.name:{affiliation}'
+        #data = get_records(query, data, max_author=3)
+        data = get_records(query, data, max_author=3, type=type)
 
-# by ROR, although we should already have those
-data = get_records('*', data, affiliation_id='https://ror.org/008xxew50')
-_store(data, f'datacite{today_str}.json')
+    # by ROR, although we should already have those
+    data = get_records('*', data, affiliation_id='https://ror.org/008xxew50', max_author=3, type=type)
+    _store(data, f'datacite_{type}{today_str}.json')
