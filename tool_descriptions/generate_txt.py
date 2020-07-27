dbf6dc1c604404ab0176c746d20aa7e35c5acac5
Brett Olivier <brettolivier@gmail.com>
2020-07-27 16:33:18 +0200

    Added text generator and data

---
+++ tool_descriptions/generate_txt.py
@@ -0,0 +1,45 @@
+import os
+import csv
+import pprint
+
+def generate_header():
+    output =\
+        """
+<html><body>
+ <h2>Long list of tools and descriptions</h2>
+ <div id="d_header">
+  <p>
+DO NOT EDIT THIS TEXT, use the tools and data file in the &quot;tool_descriptions&quot; folder on our <a href="https://github.com/vu-rdm-tech/api-scripts">api-script</a> repository to regenerate this section.<br/>
+  </p>
+ </div>
+
+ <div id="d_body">
+{}
+ </div>
+ <br/>
+</body></html>
+
+        """
+    return output
+
+def write_to_file(idata, filename):
+    doc = generate_header()
+    body = ''
+    with open(idata, 'r') as F:
+        data = csv.DictReader(F, dialect='excel-tab')
+        for row in data:
+            body += '<h3><a href="{}">{}</a> ({})</h3>\n'.format(row['URI'], row['Name'], row['Priority'])
+            if row['Description'] == 'Empty':
+                body += '<p>\n<strong>TODO:</strong> <a href={}>{}</a>\n</p>\n'.format(row['URI'], row['URI'])
+            else:
+                body += '<p>\n{}\n</p>\n'.format(row['Description'])
+            body += '<hr/>\n'
+    with open(filename, 'w') as F2:
+        F2.write(doc.format(body))
+
+if __name__ == '__main__':
+    cDir = os.path.dirname(os.path.abspath(os.sys.argv[0]))
+    input_data_file = 'tool_data.txt'
+    idata = os.path.join(cDir, input_data_file)
+    write_to_file(idata, 'doctext.html')
+
