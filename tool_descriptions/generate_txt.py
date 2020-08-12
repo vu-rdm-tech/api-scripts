88a6231961317653b02d594bef06349295aba670
Brett Olivier <brettolivier@gmail.com>
2020-08-12 11:34:42 +0200

    added source citation to text and index anchors for web presentation

--- tool_descriptions/generate_txt.py
+++ tool_descriptions/generate_txt.py
@@ -30,12 +30,21 @@ def write_to_file(idata, filename):
     body = ''
     with open(idata, 'r') as F:
         data = csv.DictReader(F, dialect='excel-tab')
+        body += '<p>\n'
+        idx = '<ol>\n'
+        for row in data:
+            idx += ' <li><a href="#tool_{}">{}</a></li>\n'.format(row['Name'].replace(' ', ''), row['Name'])
+        idx += '</ol>\n'
+        body += idx
+        body += '</p>\n'
+        F.seek(0)
+        data = csv.DictReader(F, dialect='excel-tab')
         for row in data:
             display_uri = row['URI'].replace('https://', '').replace('http://', '')
             display_uri = display_uri.split('/')[0]
             if display_uri.endswith("/"):
                 display_uri = display_uri[:-1]
-            body += '<h3>{} (<a href="{}">{}</a>)</h3>\n'.format(row['Name'], row['URI'], display_uri)
+            body += '<h3><a id="tool_{}">{}</a> (<a href="{}">{}</a>)</h3>\n'.format(row['Name'].replace(' ', ''), row['Name'], row['URI'], display_uri)
             keyword = row['Keyword']
             if keyword != '':
                 keyword = '({})'.format(keyword)
@@ -43,7 +52,14 @@ def write_to_file(idata, filename):
             if row['Description'] == 'Empty':
                 body += '<p>\n<strong>TODO:</strong> <a href={}>{}</a>\n</p>\n'.format(row['URI'], row['URI'])
             else:
-                body += '<p>\n{}\n</p>\n'.format(row['Description'])
+                body += '<p>\n{} '.format(row['Description'])
+                if len(display_uri.split('.')) > 2:
+                    src_uri = display_uri.split('.', len(display_uri.split('.'))-2)[-1]
+                else:
+                    src_uri = display_uri
+                body += '<em>This description summarised from {}.</em></p>\n'.format(src_uri)
+
+
             body += '<hr/>\n'
     with open(filename, 'w') as F2:
         F2.write(doc.format(body))
